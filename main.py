b2149fae6889ed6c64f92be3eb706cefeb5ea35d
Julia Kovalenko <juliasugarjj@gmai.com>
2025-12-27 21:22:11 +0200

    lesson7

--- main.py
+++ main.py
@@ -1,278 +1,392 @@
 import functools
 import os
+import time
+from datetime import datetime, UTC
+
+from flask import Flask, render_template, redirect, url_for, request, session
+
 import database
-
-
-from flask import Flask, render_template,redirect,url_for
-from flask import request,session
-from flask import redirect
-from flask import url_for
-from models import User
-
-from sqlalchemy import select
-from dateutil import parser
-import sqlite3
-
 import models
+from dateutil import parser
+
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-app.secret_key = os.environ.get('SECRET_KEY', 'secret string')
-
-def film_dictionary(cursor,row):
-    d={}
-    for idx,col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-class db_connection:
-    def __init__(self):
-        self.conn=sqlite3.connect('database.db')
-        self.conn.row_factory = film_dictionary
-        self.cur = self.conn.cursor()
-
-
-    def __enter__(self):
-        return self.cur
-
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.conn.commit()
-        self.conn.close()
-
-
-
-
-def get_db_result(query):
-    conn = sqlite3.connect('database.db', timeout=5)
-    conn.row_factory = film_dictionary
-    cur = conn.cursor()
-    res = cur.execute(query)
-    result = res.fetchall()
-    conn.close()
-    return result
+app.secret_key = os.environ.get("SECRET_KEY", "secret_string")
 
 
 def decorator_check_login(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        if 'login_in' in session:
-            return func (*args, **kwargs)
-        else:
-            return redirect(url_for('user_login'))
+        if session.get("logged_in"):
+            return func(*args, **kwargs)
+        return redirect(url_for("user_login"))
     return wrapper
+
 
 @app.route("/")
 @decorator_check_login
 def main_page():
-    with db_connection() as cur:
-        result = cur.execute('SELECT id,poster,name FROM film order by added_at desc limit 10').fetchall()
+    films = (
+        database.db_session
+        .query(models.Film)
+        .order_by(models.Film.added_at.desc())
+        .limit(10)
+        .all()
+    )
+    return render_template("main_page.html", films=films)
 
-    #result = get_db_result('SELECT id,poster,name FROM film order by added_at desc limit 10')
-    return render_template('main_page.html', films=result)
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
 
-@app.route('/register',methods=['GET'])
-def register_page():
-    return render_template('register.html')
+    user = models.User(
+        first_name=request.form.get("fname"),
+        last_name=request.form.get("lname"),
+        login=request.form.get("login"),
+        email=request.form.get("email"),
+        password=request.form.get("password"),
+        birth_date=parser.parse(
+            request.form.get("birth_date")
+        ).date()
+    )
 
-@app.route('/register', methods=['POST'])
-def user_register():
-    first_name = request.form['fname']
-    last_name = request.form['lname']
-    password = request.form['password']
-    login = request.form['login']
-    email = request.form['email']
-    birth_date = parser.parse(request.form['birth_date'])
-
-    database.init_db()
-    new_user = models.User(first_name=first_name, last_name=last_name, password=password, login=login, email=email, birth_date=birth_date)
-    database.db_session.add(new_user)
+    database.db_session.add(user)
     database.db_session.commit()
 
-    return 'Registered'
+    return redirect(url_for("user_login"))
 
 
-
-@app.route('/login', methods=['GET'])
+@app.route("/login", methods=["GET"])
 def user_login():
-    return  render_template('login.html')
+    return render_template("login.html")
 
 
-
-@app.route('/login', methods=['POST'])
+@app.route("/login", methods=["POST"])
 def user_login_post():
-    login = request.form['login']
-    password = request.form['password']
+    user = (
+        database.db_session
+        .query(models.User)
+        .filter(
+            models.User.login == request.form["login"],
+            models.User.password == request.form["password"]
+        )
+        .first()
+    )
+
+    if not user:
+        return "Login failed"
+
+    session["logged_in"] = True
+    session["user_id"] = user.id
+    return redirect(url_for("main_page"))
 
 
-    database.init_db()
-    stmt = select(User).where(User.login == login, models.User.password == password)
-    data = database.db_session.execute(stmt).fetchall()
-
-    result = database.db_session.query(models.User).filter_by(login=login, password=password).first()
-
-    if result:
-        session['logged_in']= True
-        session['user_id'] = result.id
-        return f'login with user {result.login}(id={result.id})'
-    return 'Login failed'
-
-
-@app.route('/logout', methods=['GET'])
-@decorator_check_login
+@app.route("/logout")
 def user_logout():
     session.clear()
-    return 'Logout'
+    return redirect(url_for("user_login"))
 
 
-@app.route('/user/<user_id>',methods=['GET', 'POST'])
-
+@app.route("/user/<int:user_id>", methods=["GET", "POST"])
 def user_profile(user_id):
-    session_user_id = session.get('user_id')
-    if request.method == 'POST':
+    user = database.db_session.get(models.User, user_id)
+    if not user:
+        return "User not found"
 
-        if int(user_id) !=session_user_id:
-            return 'You can edit only your profile'
+    if request.method == "POST":
+        if session.get("user_id") != user_id:
+            return "Forbidden"
 
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        email = request.form['email']
-        password = request.form['password']
-        birth_date = request.form['birth_date']
-        phone_number = request.form['phone_number']
-        photo =request.form['photo']
-        additional_info =request.form['additional_info']
-        with db_connection() as cur:
-            cur.execute(
-                f"UPDATE user SET first_name='{{first_name}}', last_name='{{last_name}}', email='{{email}}', password='{{password}}', birth_date='{{birth_date}}', phone='{{phone}}', photo='{{photo}}', additional_info='{{additional_info}}' WHERE id={{user_id}}")
-        return f'User{user_id} updated'
+        user.first_name = request.form["first_name"]
+        user.last_name = request.form["last_name"]
+        user.email = request.form["email"]
+        user.password = request.form["password"]
+        user.birth_date = parser.parse(
+            request.form["birth_date"]
+        ).date()
 
-    else:
-        with db_connection() as cur:
-            cur.execute(f'SELECT * FROM user WHERE id={user_id}')
-            user_by_id = cur.fetchone()
-            if session_user_id is None:
-               user_by_session = 'No user in session'
+        database.db_session.commit()
+        return redirect(url_for("user_profile", user_id=user_id))
 
-            else:
-                cur.execute(f'SELECT * FROM user WHERE id={session_user_id}')
-                user_by_session = cur.fetchone()
-        return render_template('user_page.html', user=user_by_id, user_session= user_by_session)
-    # return f'You logged in as {user_by_session}, user {user_id} data: {user_by_id}'
+    return render_template(
+        "user_page.html",
+        user=user,
+        user_session=user
+    )
 
 
-@app.route('/user/<user_id>/delete', methods=['GET'])
+@app.route("/user/<int:user_id>/delete")
 def user_delete(user_id):
-    session_user_id = session.get('user_id')
-    if user_id ==session_user_id:
-        return f'User {user_id} deleted'
-    else:
-        return 'You can delete only your profile'
+    if session.get("user_id") != user_id:
+        return "Forbidden"
+
+    user = database.db_session.get(models.User, user_id)
+    if not user:
+        return "User not found"
+
+    database.db_session.delete(user)
+    database.db_session.commit()
+    session.clear()
+
+    return redirect(url_for("user_login"))
 
 
-
-@app.route('/films', methods=['GET'])
+@app.route("/films", methods=["GET"])
 def films():
-    filter_params = request.args
-    filter_list_texts =[]
-    for key, value in filter_params.items():
-        if value:
-            if key == 'name':
-                filter_list_texts.append(f"name like '%{value}%")
-            else:
-                filter_list_texts.append(f"{key}='{value}'")
-    additional_filter =''
-    if filter_params:
-            additional_filter = 'where' + 'and'.join(filter_list_texts)
-    result = get_db_result(f'SELECT * FROM film order by added_at desc')
-    countries =get_db_result("select * from country")
-    return render_template('films.html', film = result, countries = countries)
+    query = database.db_session.query(models.Film)
+
+    if request.args.get("name"):
+        query = query.filter(
+            models.Film.name.ilike(f"%{request.args['name']}%")
+        )
+
+    if request.args.get("year"):
+        query = query.filter(
+            models.Film.year == int(request.args["year"])
+        )
+
+    if request.args.get("rating"):
+        query = query.filter(
+            models.Film.rating == int(request.args["rating"])
+        )
+
+    if request.args.get("country"):
+        query = query.filter(
+            models.Film.country == request.args["country"]
+        )
+
+    films = query.all()
+    countries = database.db_session.query(models.Country).all()
+
+    return render_template(
+        "films.html",
+        film=films,
+        countries=countries
+    )
 
 
-@app.route('/films', methods=['POST'])
-def film_add():
 
-    return 'Film added'
+@app.route("/films", methods=["POST"])
+def film_create():
+    name = request.form.get("name")
+    country = request.form.get("country")
 
-@app.route('/films/<film_id>', methods=['GET'])
-def film_info(film_id):
-    with db_connection() as cur:
-        result =cur.execute('SELECT * FROM film WHERE id = ?', (film_id,)).fetchall()
-        actors =cur.execute(f'SELECT * FROM actor join actor_film on actor.id == actor_film.actor_id where actor_film.film_id = {film_id}')
-        genres = cur.execute(f'SELECT * FROM genre_film where film_id = {film_id}').fetchall()
+    if not name:
+        return "Film name required"
 
+    film = models.Film(
+        name=name,
+        country=country,
+        year=2025,
+        rating=0,
+        added_at=int(datetime.now(UTC).timestamp())
+    )
 
-    return f'Film {film_id} is {result}, actors: {actors}, genres: {genres}'
+    database.db_session.add(film)
+    database.db_session.commit()
 
-
-
-
-@app.route('/films/<film_id>', methods=['PUT'])
-def film_update(film_id):
-    return f'Film {film_id} updated'
-
-
-@app.route('/films/<film_id>', methods=['DELETE'])
-def film_delete(film_id):
-    return f'Film {film_id} deleted'
+    return redirect(url_for("films"))
 
 
-@app.route('/films/<film_id>/rating', methods=['POST'])
-def film_rating(film_id):
-    return f'Film{film_id} rated'
+@app.route("/films/<int:film_id>", methods=["GET", "PUT", "DELETE"])
+def film_detail(film_id):
+    film = database.db_session.get(models.Film, film_id)
+    if not film:
+        return "Film not found"
+
+    if request.method == "GET":
+        actors = (
+            database.db_session
+            .query(models.Actor)
+            .join(
+                models.ActorFilm,
+                models.Actor.id == models.ActorFilm.actor_id
+            )
+            .filter(models.ActorFilm.film_id == film_id)
+            .all()
+        )
+
+        genres = (
+            database.db_session
+            .query(models.Genre)
+            .join(
+                models.GenreFilm,
+                models.Genre.genre == models.GenreFilm.genre_id
+            )
+            .filter(models.GenreFilm.film_id == film_id)
+            .all()
+        )
+
+        return render_template(
+            "film.html",
+            film=film,
+            actors=actors,
+            genres=genres
+        )
+
+    if request.method == "PUT":
+        film.name = request.form.get("name", film.name)
+        film.rating = request.form.get("rating", film.rating)
+        database.db_session.commit()
+        return "Film updated"
+
+    database.db_session.delete(film)
+    database.db_session.commit()
+    return "Film deleted"
 
 
-@app.route('/films/<film_id>/rating', methods=['GET'])
-def film_rating_info(film_id):
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    ratingRow = cur.execute(
-        f'SELECT rating FROM film WHERE id = {film_id}').fetchone()
-    return f'Film{ratingRow[0]} rating'
-
-
-@app.route('/films/<film_id>/rating', methods=['DELETE'])
-def film_rating_delete(film_id, feedback_id):
-    return f'Film {film_id} rating {feedback_id} deleted'
-
-
-@app.route('/films/<film_id>/rating/<feedback_id>/feedback_id', methods=['PUT'])
+@app.route("/films/<int:film_id>/rating/<int:feedback_id>", methods=["PUT"])
 def film_rating_update(film_id, feedback_id):
-    return f'Film {film_id} rating {feedback_id} updated'
+    feedback = (
+        database.db_session
+        .query(models.Feedback)
+        .filter(
+            models.Feedback.id == feedback_id,
+            models.Feedback.film_id == film_id
+        )
+        .first()
+    )
+
+    if not feedback:
+        return {"Feedback not found"}
+
+    if "grade" in request.form:
+        feedback.grade = int(request.form["grade"])
+
+    if "description" in request.form:
+        feedback.description = request.form["description"]
+
+    database.db_session.commit()
+
+    return {"Feedback updated"}
 
 
-@app.route('/films/<film_id>/rating/<feedback_id>/feedback', methods=['GET'])
-def film_rating_feedback(film_id, feedback_id):
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
-    feedbackRow = cur.execute(
-        f'SELECT * FROM feedback WHERE id = {feedback_id} AND film = {film_id}').fetchone()
-    return f'Film {feedbackRow}'
+@app.route(
+    "/films/<int:film_id>/rating/<int:feedback_id>",
+    methods=["DELETE"]
+)
+def film_rating_delete(film_id, feedback_id):
+    feedback = (
+        database.db_session
+        .query(models.Feedback)
+        .filter(
+            models.Feedback.id == feedback_id,
+            models.Feedback.film_id == film_id
+        )
+        .first()
+    )
+
+    if not feedback:
+        return {"Feedback not found"}
+
+    database.db_session.delete(feedback)
+    database.db_session.commit()
+
+    return {"Feedback deleted"}
 
 
-@app.route('/users/<user_id>/list', methods=['GET', 'POST'])
+@app.route("/users/<int:user_id>/list", methods=["GET", "POST"])
 def user_list(user_id):
-    return f'User {user_id} list'
+    if request.method == "GET":
+        lists = (
+            database.db_session
+            .query(models.List)
+            .filter(models.List.user_id == user_id)
+            .all()
+        )
+        return render_template(
+            "user_lists.html",
+            lists=lists,
+            user_id=user_id
+        )
+
+    name = request.form.get("name")
+    if not name:
+        return "List name required"
+
+    new_list = models.List(user_id=user_id, name=name)
+    database.db_session.add(new_list)
+    database.db_session.commit()
+
+    return redirect(url_for("user_list", user_id=user_id))
 
 
-@app.route('/users/<user_id>/list', methods=['DELETE'])
-def user_list_delete(user_id):
-    return f'User {user_id} list deleted'
-
-
-@app.route('/users/<user_id>/list/<list_id>', methods=['GET', 'POST'])
+@app.route("/users/<int:user_id>/list/<int:list_id>", methods=["GET", "POST"])
 def user_list_item(user_id, list_id):
-    return f'User {user_id} list item {list_id}'
+    user_list = (
+        database.db_session
+        .query(models.List)
+        .filter_by(id=list_id, user_id=user_id)
+        .first()
+    )
+
+    if not user_list:
+        return "List not found"
+
+    if request.method == "GET":
+        films = (
+            database.db_session
+            .query(models.Film)
+            .join(
+                models.ListFilm,
+                models.Film.id == models.ListFilm.film_id
+            )
+            .filter(models.ListFilm.list_id == list_id)
+            .all()
+        )
+
+        return render_template(
+            "user_list_items.html",
+            list=user_list,
+            films=films
+        )
+
+    film_id = request.form.get("film_id")
+    if not film_id:
+        return "Film ID required"
+
+    exists = (
+        database.db_session
+        .query(models.ListFilm)
+        .filter_by(list_id=list_id, film_id=film_id)
+        .first()
+    )
+
+    if exists:
+        return "Film already in list"
+
+    database.db_session.add(
+        models.ListFilm(list_id=list_id, film_id=film_id)
+    )
+    database.db_session.commit()
+
+    return redirect(
+        url_for("user_list_item", user_id=user_id, list_id=list_id)
+    )
 
 
-@app.route('/users/<user_id>/list/<list_id>/<film_id>', methods=['DELETE'])
+@app.route(
+    "/users/<int:user_id>/list/<int:list_id>/<int:film_id>",
+    methods=["DELETE"]
+)
 def user_list_item_delete(user_id, list_id, film_id):
-    return f'User {user_id} list item {list_id} deleted'
+    item = (
+        database.db_session
+        .query(models.ListFilm)
+        .filter_by(list_id=list_id, film_id=film_id)
+        .first()
+    )
+
+    if not item:
+        return "Film not found in list"
+
+    database.db_session.delete(item)
+    database.db_session.commit()
+
+    return "Film removed from list"
 
 
-
-
-
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,97 +1,103 @@
-from enum import unique
-
-from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, ForeignKey,Table
+from sqlalchemy.orm import relationship
 from database import Base
+
 
 class User(Base):
     __tablename__ = 'user'
+
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(50), nullable =False)
-    last_name = Column(String(50), nullable =False)
-    password = Column(String(120), nullable =False)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    password = Column(String(120), nullable=False)
     email = Column(String(120), unique=True)
-    login = Column(String(50), nullable =False, unique=True)
+    login = Column(String(50), nullable=False, unique=True)
     phone_number = Column(String(20))
     photo = Column(String(255))
     additional_info = Column(String(255))
     birth_date = Column(Date)
 
-
     def __repr__(self):
         return f'<User {self.login!r}>'
 
-    class Actor(Base):
-        __tablename__ = 'actor'
-        id = Column(Integer, primary_key=True)
-        first_name = Column(String(50), nullable=False)
-        last_name = Column(String(50), nullable=False)
-        birth_day = Column(Date)
-        death_day = Column(Date)
-        description = Column(String(255))
 
-    #def __repr__(self):
-     #   return f'<User {self.last_name!r}>'
+class Actor(Base):
+    __tablename__ = 'actor'
 
-    class Film(Base):
-        __tablename__ = 'film'
-        id = Column(Integer, primary_key=True)
-        name = Column(String(50), nullable=False)
-        year = Column(Integer, nullable=False)
-        poster = Column(String(255))
-        description = Column(String(255))
-        rating = Column(Integer, nullable=False)
-        duration = Column(Integer)
-        added_at = Column(Integer, nullable=False)
-        country = Column(String(50), nullable=False)
-
-        def __repr__(self):
-            return f'<User {self.name!r}>'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    birth_day = Column(Date)
+    death_day = Column(Date)
+    description = Column(String(255))
 
 
-    class Genre(Base):
-            __tablename__ = 'genre'
-            genre= Column(String(50), nullable=False, primary_key=True)
+class Film(Base):
+    __tablename__ = 'film'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    year = Column(Integer, nullable=False)
+    poster = Column(String(255))
+    description = Column(String(255))
+    rating = Column(Integer, nullable=False)
+    duration = Column(Integer)
+    added_at = Column(Integer, nullable=False)
+    country = Column(String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Film {self.name!r}>'
 
 
-   # def __repr__(self):
-    #    return f'<User {self.genre!r}>'
+class Genre(Base):
+    __tablename__ = 'genre'
+
+    genre = Column(String(50), primary_key=True)
 
 
-    class GenreFilm(Base):
-            __tablename__ = 'genre_film'
-            id = Column(Integer, primary_key=True)
-            genre_id= Column(Integer, ForeignKey('genre.genre'))
-            film_id = Column(Integer, ForeignKey('film.id'))
+class GenreFilm(Base):
+    __tablename__ = 'genre_film'
+
+    id = Column(Integer, primary_key=True)
+    genre_id = Column(String(50), ForeignKey('genre.genre'))
+    film_id = Column(Integer, ForeignKey('film.id'))
 
 
-    class ActorFilm(Base):
-            __tablename__ = 'actor_film'
-            id = Column(Integer, primary_key=True)
-            actor_id= Column(Integer, ForeignKey('actor.id'))
-            film_id = Column(Integer, ForeignKey('film.id'))
+class ActorFilm(Base):
+    __tablename__ = 'actor_film'
 
-    class List(Base):
-        __tablename__ = 'list'
-        id = Column(Integer, primary_key=True)
-        user_id = Column(Integer, ForeignKey('user.id'))
-        name = Column(String(50), nullable=False)
-
-    class ListFilm(Base):
-             __tablename__ = 'list_film'
-             id = Column(Integer, primary_key=True)
-             list_id = Column(Integer, ForeignKey('list.id'))
-             film_id = Column(Integer, ForeignKey('film.id'))
-
-    class Feedback(Base):
-        __tablename__ = 'feedback'
-        id = Column(Integer, primary_key=True)
-        film_id = Column(Integer, ForeignKey('film.id'))
-        user_id = Column(Integer, ForeignKey('user.id'))
-        grade = Column(Integer)
-        description = Column(String(255))
+    id = Column(Integer, primary_key=True)
+    actor_id = Column(Integer, ForeignKey('actor.id'))
+    film_id = Column(Integer, ForeignKey('film.id'))
 
 
-    class Country(Base):
-        __tablename__ = 'country'
-        country_name = Column(String(50), primary_key=True, unique = True)
+class List(Base):
+    __tablename__ = 'list'
 
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    name = Column(String(50), nullable=False)
+
+
+class ListFilm(Base):
+    __tablename__ = 'list_film'
+
+    id = Column(Integer, primary_key=True)
+    list_id = Column(Integer, ForeignKey('list.id'))
+    film_id = Column(Integer, ForeignKey('film.id'))
+
+
+class Feedback(Base):
+    __tablename__ = 'feedback'
+
+    id = Column(Integer, primary_key=True)
+    film_id = Column(Integer, ForeignKey('film.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    grade = Column(Integer)
+    description = Column(String(255))
+
+
+class Country(Base):
+    __tablename__ = 'country'
+
+    country_name = Column(String(50), primary_key=True, unique=True)
