61f54cf1a2bab10097bb32101944dc492648c69f
Julia Kovalenko <juliasugarjj@gmai.com>
2026-01-18 21:10:52 +0200

    lesson_8

--- database.py
+++ database.py
@@ -3,12 +3,12 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.orm import declarative_base
 
 
-engine = create_engine('sqlite:///database.db', echo=True)
+engine = create_engine('sqlite:///database_db', echo=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-def init_db():
-   Base.metadata.create_all(bind=engine)
+ #def init_db():
+ #  Base.metadata.create_all(bind=engine)
